fe3fdacda87f4616ef26b8f49222fa497b6f0145
PSigfridsson <petter.sigfridsson@gmail.com>
2019-02-13 01:56:42 +0100

    Fixed javascript part of lab?

---
+++ public/js/menu.js
@@ -0,0 +1,42 @@
+var food = [
+  {
+    "name": "The Fire Burger",
+    "kCal": 2200,
+    "lactose": true,
+    "gluten": true,
+    "ingr": ["Kobe beef of the highest quality", "Covered in fancy cheese", "Cold-leavened sourdough bread"],
+    "img": "https://images4.alphacoders.com/871/871251.jpg"
+  },
+  {
+    "name": "Fried Turkey Burger",
+    "kCal": 1600,
+    "lactose": true,
+    "gluten": false,
+    "ingr": ["Deep fried Turkish turke", "A slice of pink stuff", "Shake-n-bake gluten-free bread"],
+    "img": "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/master/pass/shutterstock_148839410.jpg"
+  },
+  {
+    "name": "A Double w/ cheese",
+    "kCal": 1300,
+    "lactose": true,
+    "gluten": true,
+    "ingr": ["Two layers of pure meat", "Lots of yummy cheese", "Sweet bread bun"],
+    "img": "https://www.foodora.se/wp-content/uploads/sites/11/2016/12/janis-tripping-mushroom-burger-1920x1280.jpg"
+  },
+  {
+    "name": "Mordor burger",
+    "kCal": 1300,
+    "lactose": true,
+    "gluten": true,
+    "ingr": ["Two layers of pure meat", "Lots of yummy cheese", "Sweet bread bun"],
+    "img": "https://www.foodora.se/wp-content/uploads/sites/11/2016/12/janis-tripping-mushroom-burger-1920x1280.jpg"
+  },
+  {
+    "name": "Snake burger",
+    "kCal": 1300,
+    "lactose": true,
+    "gluten": true,
+    "ingr": ["Two layers of pure meat", "Lots of yummy cheese", "Sweet bread bun"],
+    "img": "https://www.foodora.se/wp-content/uploads/sites/11/2016/12/janis-tripping-mushroom-burger-1920x1280.jpg"
+  },
+]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,18 +1,55 @@
-let burger1 = new MenuItem("The Fire Burger", 2200, true, true, ["Kobe beef of the highest quality", "Covered in fancy cheese", "Cold-leavened sourdough bread"], "https://images4.alphacoders.com/871/871251.jpg", "box a");
-let burger2 = new MenuItem("Fried Turkey Burger", 1600, true, false, ["Deep fried Turkish turke", "A slice of pink stuff", "Shake-n-bake gluten-free bread"], "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/master/pass/shutterstock_148839410.jpg", "box b");
-let burger3 = new MenuItem("A Double w/ cheese", 1300, true, true, ["Two layers of pure meat", "Lots of yummy cheese", "Sweet bread bun"], "https://www.foodora.se/wp-content/uploads/sites/11/2016/12/janis-tripping-mushroom-burger-1920x1280.jpg", "box c");
-let burger4 = new MenuItem("A Double w/ cheese", 1300, true, true, ["Two layers of pure meat", "Lots of yummy cheese", "Sweet bread bun"], "https://www.foodora.se/wp-content/uploads/sites/11/2016/12/janis-tripping-mushroom-burger-1920x1280.jpg", "box c");
-
 var vm = new Vue({
-    el: '#Burgers',
-    data: {
-        burgers: [
-            burger1,
-            burger2,
-            burger3,
-            burger4,
-            burger4
-            
-        ]
+  el: '#Burgers',
+  data: {
+    burgers: food,
+    checkedBurgers: []
+  }
+})
+
+var vm1 = new Vue({
+  el: '#buttonArea',
+  data: {
+    customerInfo: []
+  },
+  methods: {
+    submitForm: function (event) {
+
+      document.getElementById("formData").innerHTML = "";
+
+      var name = document.getElementById("full name").value;
+      var email = document.getElementById("email").value;
+      var street = document.getElementById("street").value;
+      var house = document.getElementById("house").value;
+
+      // checking radio buttons.
+      var radios = document.getElementsByName('gender');
+      for (var i = 0, length = radios.length; i < length; i++)
+      {
+        if (radios[i].checked)
+        {
+          var gender = radios[i].value;
+          break;
+        }
+      }
+
+      var paymentOption = document.getElementById("recipient").value;
+      // Checking checkboxes for burgers
+      var burgers = vm.checkedBurgers;
+      if(vm.checkedBurgers.length == 0){
+        alert("No burger/burgers are choosen");        
+      }
+      // Sets the array of custommerInfo
+      vm1.customerInfo = [name, email, street, house, paymentOption, gender, burgers];
+
+      // Checks if the form is correctly filled.
+      for (i = 0; i < vm1.customerInfo.length - 1; i++) {
+        if(vm1.customerInfo[i].length == 0){
+          alert("You need to fill in the form correctly")
+          //vm1.customerInfo = [];
+          return;
+        }
+      }
+      document.getElementById("formData").innerHTML = vm1.customerInfo;
     }
+  }
 })
